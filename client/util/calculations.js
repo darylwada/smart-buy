5f069524ff49800fe8f48402d2a36072dae915cc
darylwada <daryl.wada@gmail.com>
2018-07-24 15:04:51 -0700

    Fixed logic issues with calculations. #2

--- client/util/calculations.js
+++ client/util/calculations.js
@@ -12,18 +12,14 @@ function calculateMortgagePayment(principal, interestRate) {
 
 function forecastDebt(principal, interestRate, mortgagePayment) {
   const debt = [principal]
-  const monthlyPrincipal = []
-  const monthlyInterest = []
-  for (let i = 0; i <= 360; i++) {
-    monthlyInterest.push(debt[i] * interestRate / 12)
+  const monthlyPrincipal = [0]
+  const monthlyInterest = [0]
+  for (let i = 1; i <= 360; i++) {
+    monthlyInterest.push(debt[i - 1] * interestRate / 12)
     monthlyPrincipal.push(mortgagePayment - monthlyInterest[i])
-    debt.push(Math.max(debt[i] - monthlyPrincipal[i], 0))
+    debt.push(Math.max(debt[i - 1] - monthlyPrincipal[i], 0))
   }
-  return {
-    debtInterest: monthlyInterest,
-    paidPrincipal: monthlyPrincipal,
-    debt: debt
-  }
+  return debt
 }
 
 function forecastEquity(homeValue, debt) {
@@ -37,7 +33,7 @@ function forecastSellingFees(homeValue, salesCommission) {
 function forecastMonthlyEquity(purchasePrice, annualAppreciation, principal, interestRate, salesCommission) {
   const homeValue = forecastHomeValue(purchasePrice, annualAppreciation)
   const mortgagePayment = calculateMortgagePayment(principal, interestRate)
-  const { debt } = forecastDebt(principal, interestRate, mortgagePayment)
+  const debt = forecastDebt(principal, interestRate, mortgagePayment)
   const equity = forecastEquity(homeValue, debt)
   const fees = forecastSellingFees(homeValue, salesCommission)
   const netEquity = equity.map((value, i) => value - fees[i])
@@ -52,9 +48,10 @@ function forecastMonthlyEquity(purchasePrice, annualAppreciation, principal, int
 
 export function forecastAnnualEquity({ purchasePrice, annualAppreciation, downPayment, interestRate, salesCommission }) {
   const principal = purchasePrice * (1 - downPayment)
-  const buyForecast = forecastMonthlyEquity(purchasePrice, annualAppreciation, principal, interestRate, salesCommission)
-  for (const key in buyForecast) {
-    buyForecast[key] = buyForecast[key].filter((value, i) => i % 12 === 0).map(num => parseInt(num, 10))
+  const monthlyForecast = forecastMonthlyEquity(purchasePrice, annualAppreciation, principal, interestRate, salesCommission)
+  const annualForecast = {}
+  for (const metric in monthlyForecast) {
+    annualForecast[metric] = monthlyForecast[metric].filter((value, i) => i % 12 === 0).map(num => parseInt(num, 10))
   }
-  return buyForecast
+  return annualForecast
 }
