e73dfed1b916cac3612df11960d632e94f3761dd
darylwada <daryl.wada@gmail.com>
2018-07-26 20:03:36 -0700

    Added a scenarios button to the nav bar. #7

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,14 +1,23 @@
 import React from 'react'
-import { Row, Navbar, NavbarBrand } from 'reactstrap'
+import { Row, Col, NavbarBrand, NavLink } from 'reactstrap'
+
+const styles = {
+  row: {
+    height: '60px'
+  }
+}
 
 export default function Header() {
   return (
-    <Row className="bg-white border shadow-sm">
-      <Navbar>
+    <Row className="bg-white border shadow-sm align-items-center" style={styles.row}>
+      <Col md="6">
         <NavbarBrand className="font-weight-bold" href="/">
           <i className="fas fa-home fa-lg mr-1"></i>SmartBuy
         </NavbarBrand>
-      </Navbar>
+      </Col>
+      <Col md="6">
+        <NavLink className="d-flex justify-content-end" href="/">Scenarios</NavLink>
+      </Col>
     </Row>
   )
 }
