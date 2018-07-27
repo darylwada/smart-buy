e93df9ba70d6cfa182f0dcd433b78c9c596f940c
darylwada <daryl.wada@gmail.com>
2018-07-27 12:55:24 -0700

    Added routes for get requests to return the requested scenario. #8

--- server/routes/router.js
+++ server/routes/router.js
@@ -5,6 +5,25 @@ module.exports = function sencariosRouter(collection) {
 
   const router = new Router()
 
+  router.get('/', (req, res, next) => {
+    collection
+      .find()
+      .toArray()
+      .then(scenarios => res.json(scenarios))
+      .catch(err => next(err))
+  })
+
+  router.get('/:id', (req, res, next) => {
+    collection
+      .findOne({ id: req.params.id })
+      .then(found => {
+        found
+          ? res.json(found)
+          : res.sendStatus(404)
+      })
+      .catch(err => next(err))
+  })
+
   router.post('/', (req, res, next) => {
     const scenario = Object.assign(req.body, { id: uuid() })
     collection
