4d46c2d0798b3147bcec60405d446994f141bd08
darylwada <daryl.wada@gmail.com>
2018-08-01 11:31:48 -0700

    Added minimize functionality for the summary. #11

--- client/components/Summary.js
+++ client/components/Summary.js
@@ -1,30 +1,52 @@
-import React, { Fragment } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Row, Col, Collapse } from 'reactstrap'
 import getParagraph from '../util/get-paragraph';
 
 const styles = {
+  header: {
+    padding: '0 50px'
+  },
   row: {
     minHeight: '250px',
     padding: '0 50px'
   }
 }
 
-export default function Summary(props) {
-  const { netEquity, investment } = props.data
-  const { purchasePrice, downPayment, closingCosts, rentReturn, annualAppreciationRate } = props.inputs
-  const downPaymentAmt = (purchasePrice * downPayment / 100).toLocaleString()
-  const closingCostsAmt = (purchasePrice * closingCosts / 100).toLocaleString()
-  const paragraph = getParagraph(netEquity, investment, downPaymentAmt, closingCostsAmt, rentReturn, annualAppreciationRate)
+export default class Summary extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { collapse: false }
+  }
 
-  return (
-    <Fragment>
-    <i className="fas fa-window-minimize" id="summary-toggler" style={{ cursor: 'pointer' }}></i>
-    <Row style={styles.row}>
-      <Col md="12">
-        <h5 className="mb-3">Summary</h5>
-        {paragraph}
-      </Col>
-    </Row>
-    </Fragment>
-  )
+  toggle = () => {
+    this.setState({ collapse: !this.state.collapse })
+  }
+
+  render() {
+    const { netEquity, investment } = this.props.data
+    const { purchasePrice, downPayment, closingCosts, rentReturn, annualAppreciationRate } = this.props.inputs
+    const { collapse } = this.state
+    const downPaymentAmt = (purchasePrice * downPayment / 100).toLocaleString()
+    const closingCostsAmt = (purchasePrice * closingCosts / 100).toLocaleString()
+    const paragraph = getParagraph(netEquity, investment, downPaymentAmt, closingCostsAmt, rentReturn, annualAppreciationRate)
+    const icon = collapse
+      ? 'far fa-plus-square'
+      : 'far fa-minus-square'
+
+    return (
+      <Fragment>
+        <Row style={styles.header}>
+          <h5 className="mb-3 mr-2">Summary</h5>
+          <i className={icon} style={{ cursor: 'pointer' }} onClick={this.toggle}></i>
+        </Row>
+        <Collapse isOpen={!this.state.collapse}>
+          <Row style={styles.row}>
+            <Col md="12">
+              {paragraph}
+            </Col>
+          </Row>
+        </Collapse>
+      </Fragment>
+    )
+  }
 }
