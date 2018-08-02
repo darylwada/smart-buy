302884fbdd96f12815330e900e893e5d30ce012a
darylwada <daryl.wada@gmail.com>
2018-08-02 15:13:07 -0700

    Refactored to have global variables with which to test. #32

--- client/util/calculations.test.js
+++ client/util/calculations.test.js
@@ -1,47 +1,67 @@
 import calculations from './calculations'
 
+const purchasePrice = 500000
+const interestRate = 0.045
+const downPayment = 0.2
+const closingCosts = 0.03
+const salesCommission = 0.06
+const propertyTaxRate = 0.0125
+const hoa = 400
+const maintenance = 100
+const insurance = 100
+const annualAppreciationRate = 0.03
+const incomeTaxRate = 0.25
+const generalInflationRate = 0.02
+const rentBase = 2500
+const rentInflationRate = 0.02
+const rentReturn = 0.06
+const principal = 400000
+
+const homeValue = calculations.forecastHomeValue(purchasePrice, annualAppreciationRate)
+const mortgagePayment = calculations.calculateMortgagePayment(principal, interestRate)
+const { debt, paidInterest, paidPrincipal } = calculations.forecastDebt(principal, interestRate, mortgagePayment)
+const equity = calculations.forecastEquity(homeValue, debt)
+const fees = calculations.forecastSellingFees(homeValue, salesCommission)
+const netEquity = equity.map((value, i) => value - fees[i])
+const propertyTax = calculations.forecastPropertyTax(homeValue, propertyTaxRate)
+const expenses = calculations.forecastExpenses(propertyTax, hoa, maintenance, insurance, generalInflationRate)
+const deductions = calculations.forecastDeductions(paidInterest, propertyTax, incomeTaxRate)
+const cashFlow = calculations.forecastCashFlow(deductions, mortgagePayment, expenses)
+const rent = calculations.forecastRent(rentBase, rentInflationRate)
+const savings = calculations.forecastSavings(cashFlow, rent)
+const investment = calculations.forecastRentInvestment(downPayment, closingCosts, purchasePrice, rentReturn, savings)
+
 xdescribe('Home value', () => {
-  it('Returns forecasted home value give a purchase price and an appreciation rate.', () => {
-    const purchasePrice = 500000
-    const annualAppreciationRate = 0.02
-    const result = calculations.forecastHomeValue(purchasePrice, annualAppreciationRate)
-    expect(result[0]).toBe(purchasePrice)
-    expect(Math.round(result[1])).toBe(500833)
-    expect(Math.round(result[length - 1])).toBe(910604)
-  })
-  it('Returns the purchase price if the appreciation rate is zero.', () => {
-    const purchasePrice = 500000
-    const annualAppreciationRate = 0
-    const result = calculations.forecastHomeValue(purchasePrice, annualAppreciationRate)
-    expect(result[0]).toBe(purchasePrice)
-    expect(result[1]).toBe(purchasePrice)
-    expect(result[length - 1]).toBe(purchasePrice)
+  it('Calculates the home value correctly.', () => {
+    expect(homeValue[0]).toBe(purchasePrice)
+    expect(Math.round(homeValue[1])).toBe(501250)
+    expect(Math.round(homeValue[homeValue.length - 1])).toBe(1228421)
   })
 })
 
 xdescribe('Mortgage payment', () => {
-  it('Return the correct value for a mortgage payment given a principal and interest rate', () => {
-    const principal = 300000
-    const interestRate = 0.0425
-    const result = calculations.calculateMortgagePayment(principal, interestRate)
-    expect(Math.round(result)).toBe(1476)
+  it('Calculates the monthly mortgage payment correctly.', () => {
+    expect(Math.round(mortgagePayment)).toBe(2027)
   })
 })
 
-describe('Debt forecast', () => {
-  it('Returns the correct values for remaining debt, paid principal, and paid interest.', () => {
-    const principal = 300000
-    const interestRate = 0.0425
-    const mortgagePayment = calculations.calculateMortgagePayment(principal, interestRate)
-    const { debt, paidInterest, paidPrincipal } = calculations.forecastDebt(principal, interestRate, mortgagePayment)
+xdescribe('Debt forecast', () => {
+  it('Calculates remaining debt, paid principal, and paid interest correctly.', () => {
     expect(debt[0]).toBe(principal)
     expect(paidInterest[0]).toBe(0)
     expect(paidPrincipal[0]).toBe(0)
-    expect(Math.round(debt[1])).toBe(299587)
-    expect(Math.round(paidInterest[1])).toBe(1063)
-    expect(Math.round(paidPrincipal[1])).toBe(413)
+    expect(Math.round(debt[1])).toBe(399473)
+    expect(Math.round(paidInterest[1])).toBe(1500)
+    expect(Math.round(paidPrincipal[1])).toBe(527)
     expect(Math.round(debt[debt.length - 1])).toBe(0)
-    expect(Math.round(paidInterest[paidInterest.length - 1])).toBe(5)
-    expect(Math.round(paidPrincipal[paidPrincipal.length - 1])).toBe(1471)
+    expect(Math.round(paidInterest[paidInterest.length - 1])).toBe(8)
+    expect(Math.round(paidPrincipal[paidPrincipal.length - 1])).toBe(2019)
+  })
+})
+
+describe('Equity forecast', () => {
+  it('Calculates home equity correctly.', () => {
+    expect(Math.round(equity[0])).toBe(100000)
+    expect(Math.round(equity[1])).toBe(101777)
   })
 })
