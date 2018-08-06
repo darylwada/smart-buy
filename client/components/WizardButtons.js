409320ed49da217e0fc0c600d4f02c3cabbbdc02
Daryl Wada <daryl.wada@gmail.com>
2018-08-06 13:29:15 -0700

    Chart section height now adjusts when the chart shrinks. #36

--- client/components/WizardButtons.js
+++ client/components/WizardButtons.js
@@ -15,7 +15,7 @@ export default function WizardButtons({ activeTab, handleButtonClick }) {
   if (activeTab === 'rent') showNext = false
   return (
     <Fragment>
-    <Col xs="6">
+    <Col xs="6" className="mt-3 mt-md-0">
       <Button 
         id="previous" 
         className={showPrevious ? 'visible' : 'invisible'} 
@@ -24,7 +24,7 @@ export default function WizardButtons({ activeTab, handleButtonClick }) {
         Previous
     </Button>
     </Col>
-    <Col xs="6" className="d-flex justify-content-end">
+    <Col xs="6" className="d-flex justify-content-end mt-3 mt-md-0">
       <Button 
         id="next" 
         className={showNext ? 'visible' : 'invisible'} 
