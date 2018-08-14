1c9bacc1515a4d4c9d0d220a1904d13099c28213
darylwada <daryl.wada@gmail.com>
2018-08-14 14:26:47 -0700

    Created a route for the login request and incorporated login validation. #35

--- server/routes/auth-router.js
+++ server/routes/auth-router.js
@@ -1,5 +1,6 @@
 const uuid = require('uuid')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const { Router } = require('express')
 
 const validateCredentials = (credentials = {}) => {
@@ -30,6 +31,16 @@ module.exports = function authRouter(users) {
       .catch(err => next(err))
   }
 
+  const validateLogin = (req, res, next) => {
+    const errors = validateCredentials(req.body)
+    if (Object.keys(errors).length) {
+      return res.status(401).json({
+        error: 'Invalid login.'
+      })
+    }
+    next()
+  }
+
   const router = new Router()
 
   router.post('/sign-up', validateSignUp, (req, res, next) => {
@@ -40,6 +51,32 @@ module.exports = function authRouter(users) {
       .then(password => users.insertOne({ id, username, password }))
       .then(() => users.findOne({ id }, { projection: { password: 0 } }))
       .then(user => res.status(201).json(user))
+      .catch(err => next(err))
+  })
+
+  router.post('/login', validateLogin, (req, res, next) => {
+    const { body: { username, password: unhashed } } = req
+    users
+      .findOne({ username })
+      .then(found => {
+        if (!found) return null
+        const { id, password: hashed } = found
+        return bcrypt
+          .compare(unhashed, hashed)
+          .then(match => {
+            if (!match) return null
+            return { id, username }
+          })
+      })
+      .then(user => {
+        if (!user) {
+          return res.status(401).json({
+            error: 'Invalid login.'
+          })
+        }
+        const token = jwt.sign(user, process.env.TOKEN_SECRET)
+        res.status(201).json(Object.assign(user, { token }))
+      })
       .catch(err => next(err))
   })
 
